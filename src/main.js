f91e1e34b5a72dfd7d11d9d2995d390619b6a0c1
yestodorrow <power998@163.com>
2018-03-28 19:37:09 +0800

    新增了代理检测的组件，测试chrome、firefox。opera

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Client from './plugins'
+
+
+
 
 import AudioVisual from 'vue-audio-visual'
  
